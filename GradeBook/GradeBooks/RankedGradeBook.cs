a7841eeb1ce5f9aab85e71349d952ad4e5351d3a
MarkFoad <markf@dahallco.com.au>
2018-11-08 19:13:51 +1000

    completted module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -17,7 +17,7 @@ namespace GradeBook.GradeBooks
                 throw new InvalidOperationException("Ranked grading requires at least 5 students.");
 
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
-            var grades = Students.OrderByDescending(e => e.AddGrade).Select(e => e.AverageGrade).ToList();
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
             if (grades[threshold - 1] <= averageGrade)
                 return 'A';
